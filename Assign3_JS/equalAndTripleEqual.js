f737f6d74060d082433451297a0641b22427e57a
she0930she <she0930she@hotmail.com>
2024-03-06 18:23:20 -0800

    upload JS file

---
+++ Assign3_JS/equalAndTripleEqual.js
@@ -0,0 +1,19 @@
+let x = 10;
+let y = '10';
+
+console.log(x == y);  // Output: true (values are equal after type coercion)
+console.log(x === y); // Output: false (values are not of the same type)
+
+let a = 3
+let a2 = 1
+let b = true;
+
+console.log(a == b, "3 == true");  // Output: true (values are equal after type coercion)
+console.log(a2 == b, "1 == true")
+console.log(a === b); // Output: false (values are not of the same type)
+
+let c = 0;
+let d = false;
+
+console.log(c == d, "ans: true")
+console.log(c === d, "ans: false")
---
+++ Assign3_JS/objPrac.js
@@ -0,0 +1,77 @@
+// Problem 1
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+    }
+
+function sumAllSalaries(obj){
+    let tol = 0
+    for (let key in salaries){
+        tol += salaries[key]
+    }
+    return tol
+}
+
+//console.log(sumAllSalaries(salaries))
+
+
+// Problem 2
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+    };
+
+function multiplyNumeric(obj){
+    //console.log(menu)
+    for (let key in obj){
+        if (typeof(obj[key]) === "number"){ // check type use triple equal
+            obj[key] = obj[key] * 2
+        }
+    }
+}
+
+// multiplyNumeric(menu)
+// console.log(menu)
+
+
+
+// Problem 3
+
+function checkEmailId(strWords){
+    let firstIdx = -1
+    let secondIdx = -1
+    for (let idx in strWords){
+        if (strWords[idx] === "@"){
+            firstIdx = idx;
+        }
+        if (strWords[idx] === "." && firstIdx != -1 && (idx - firstIdx >= 2)){ // difference >= 2
+            secondIdx = idx
+        }
+    }
+    if (firstIdx != -1 && secondIdx != -1) return true;
+    return false;
+
+}
+// console.log(checkEmailId("geg@.gew"), "F")
+// console.log(checkEmailId("geg@zzz.gew"), "T")
+// console.log(checkEmailId("geg@gew"), "F")
+// console.log(checkEmailId("geg.gew"), "F")
+
+
+function checkEmailIdUsingRegex(str) {
+    // Regular expression to match the email pattern
+    const regex = /^[^@]+@[^@.]+\.[^@.]+$/i;
+  
+    // Test the string against the regular expression
+    return regex.test(str);
+  }
+  
+  // Test cases
+  console.log(checkEmailIdUsingRegex("example@gmail.com")); // Output: true
+  console.log(checkEmailIdUsingRegex("user123@yahoo.co.uk")); // Output: true
+  console.log(checkEmailIdUsingRegex("invalidemail@.com")); // Output: false
+  console.log(checkEmailIdUsingRegex("anotherinvalidemail.com")); // Output: false
+  console.log(checkEmailIdUsingRegex("email@domain")); // Output: false
+  
